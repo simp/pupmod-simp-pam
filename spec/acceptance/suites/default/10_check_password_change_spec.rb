0b64a7539dce2dfe2c8479f03cd0fb01215b5395
Nick Miller <nick.miller@onyxpoint.com>
2018-07-12 14:22:10 -0400

    Fix acceptance test

--- spec/acceptance/suites/default/10_check_password_change_spec.rb
+++ spec/acceptance/suites/default/10_check_password_change_spec.rb
@@ -31,100 +31,100 @@ describe 'pam check password change' do
         it 'should reject passwords that are too short' do
           stdin = "suP3rF!0B\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/is too simple/)
           else
-            expect(result.stderr).to match /password is shorter than 15 characters/
+            expect(result.stderr).to match(/password is shorter than 15 characters/)
           end
         end
 
         it 'should reject passwords that do not contain numbers' do
           stdin = "suPerFooB@rB@z%xAB\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/is too simple/)
           else
-            expect(result.stderr).to match /password contains less than 1 digit/
+            expect(result.stderr).to match(/password contains less than 1 digit/)
           end
         end
 
         it 'should reject passwords that do not contain lower case letters' do
           stdin = "E723X00^@3Z8@67X23\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match (/is too simple/)
           else
-            expect(result.stderr).to match /password contains less than 1 lowercase letter/
+            expect(result.stderr).to match (/password contains less than 1 lowercase letter/)
           end
         end
 
         it 'should reject passwords that do not contain upper case letters' do
           stdin = "su2er3oo5@r2@z%x23\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/is too simple/)
           else
-            expect(result.stderr).to match /password contains less than 1 uppercase letter/
+            expect(result.stderr).to match(/password contains less than 1 uppercase letter/)
           end
         end
 
         it 'should reject passwords that do not contain symbols' do
           stdin = "su2er3oo52r2Xz2x23\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/is too simple/)
           else
-            expect(result.stderr).to match /password contains less than 1 non-alphanumeric character/
+            expect(result.stderr).to match(/password contains less than 1 non-alphanumeric character/)
           end
         end
 
         it 'should reject passwords that have too many characters from the same character class' do
           stdin = "supeRFooB@rB@z%x23\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /is too simple/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/is too simple/)
           else
-            expect(result.stderr).to match /password contains more than 3 characters of the same class consecutively/
+            expect(result.stderr).to match(/password contains more than 3 characters of the same class consecutively/)
           end
         end
 
         it 'should reject passwords that have too many repeating characters' do
           stdin = "suPerFoooB@rB@z%x23\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /contains too many same characters consecutively/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/contains too many same characters consecutively/)
           else
-            expect(result.stderr).to match /password contains more than 2 same characters consecutively/
+            expect(result.stderr).to match(/password contains more than 2 same characters consecutively/)
           end
         end
 
         it 'should reject passwords that have a monotonic character sequence that is too long' do
-          if host.name == 'el7'
+          if host.name =~ /el7/
             skip 'unable to cause failure on el7 without first hitting other password restrictions'
           end
 
           stdin = "123=456=789=0Qw\n"*repeat_when_failure       # el6: 'too simplistic/systematic'
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          expect(result.stderr).to match /is too simplistic\/systematic/
+          expect(result.stderr).to match(/is too simplistic\/systematic/)
         end
 
         it 'should reject passwords containing the username' do
           stdin = "suPerFo0B@rB@z%#{test_user}\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /contains the user name in some form/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/contains the user name in some form/)
           else
-            expect(result.stderr).to match /password contains the user name in some form/
+            expect(result.stderr).to match(/password contains the user name in some form/)
           end
         end
 
         it 'should reject passwords containing part of the GECOS' do
           stdin = "Tst0$uPerFo0B@rB@z%\n"*repeat_when_failure
           result = on(host, "passwd #{test_user}", {:stdin => stdin, :acceptable_exit_codes => [1]})
-          if host.name == 'el6'
-            expect(result.stderr).to match /contains the user name in some form/
+          if host.name =~ /el6/
+            expect(result.stderr).to match(/contains the user name in some form/)
           else
-            expect(result.stderr).to match /password contains words from the real name of the user in some form/
+            expect(result.stderr).to match(/password contains words from the real name of the user in some form/)
           end
         end
       end
