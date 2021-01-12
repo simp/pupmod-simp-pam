f5b9000ecf6fc452514ebe20663eb7aa301df084
Trevor Vaughan <tvaughan@onyxpoint.com>
2021-01-12 11:12:59 -0500

    adjust acceptance tests for oel

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -41,7 +41,10 @@ describe 'pam class' do
           end
         else
           it 'should not replace authconfig and authselect should do nothing if not forced' do
-            on(host, 'ls -l /usr/sbin/authconfig', :acceptable_exit_codes => [2] )
+            # OEL symlinks this internally
+            result = on(host, 'ls -l /usr/sbin/authconfig', :accept_all_exit_codes => true)
+            expect(result.stdout).not_to match(/simp_auth/)
+
             result = on(host,'/usr/bin/authselect select sssd', :accept_all_exit_codes => true)
             expect(result.stderr).to match(/Refusing to activate profile/)
             apply_manifest_on(host, manifest, {:catch_changes => true})
