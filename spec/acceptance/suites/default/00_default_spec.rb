3c6d7f2eb55ac0915526166249587854a8aa9b9f
jeannegreulich <jeanne.greulich@onyxpoint.com>
2021-01-14 18:16:06 -0500

    remove EL6 tests from acceptance tests

--- spec/acceptance/suites/default/00_default_spec.rb
+++ spec/acceptance/suites/default/00_default_spec.rb
@@ -20,35 +20,14 @@ describe 'pam class' do
           apply_manifest_on(host, manifest, {:catch_changes => true})
         end
 
-        case  host[:platform]
-        when /el-[67]-x86_64/
-          it 'should replace authconfig and authconfi-tui links' do
-            result = on(host, 'ls -l /usr/sbin/authconfig')
-            expect(result.stdout).to match(/authconfig -> \/usr\/local\/sbin\/simp_authconfig.sh/)
+        it 'should not replace authconfig and authselect should do nothing if not forced' do
+          # OEL symlinks this internally
+          result = on(host, 'ls -l /usr/sbin/authconfig', :accept_all_exit_codes => true)
+          expect(result.stdout).not_to match(/simp_auth/)
 
-            result = on(host, 'ls -l /usr/sbin/authconfig-tui')
-            expect(result.stdout).to match(/authconfig-tui -> \/usr\/local\/sbin\/simp_authconfig.sh/)
-          end
-
-          it 'authconfig and authconfig-tui should have no affect on PAM configuration' do
-            on(host, '/usr/sbin/authconfig --update')
-            # verify Puppet detects no changes to PAM configuration after authconfig is run
-            apply_manifest_on(host, manifest, {:catch_changes => true})
-
-            # verify Puppet detects no changes to PAM configuration after authconfig-tui is run
-            on(host, '/usr/sbin/authconfig-tui --update')
-            apply_manifest_on(host, manifest, {:catch_changes => true})
-          end
-        else
-          it 'should not replace authconfig and authselect should do nothing if not forced' do
-            # OEL symlinks this internally
-            result = on(host, 'ls -l /usr/sbin/authconfig', :accept_all_exit_codes => true)
-            expect(result.stdout).not_to match(/simp_auth/)
-
-            result = on(host,'/usr/bin/authselect select sssd', :accept_all_exit_codes => true)
-            expect(result.stderr).to match(/Refusing to activate profile/)
-            apply_manifest_on(host, manifest, {:catch_changes => true})
-          end
+          result = on(host,'/usr/bin/authselect select sssd', :accept_all_exit_codes => true)
+          expect(result.stderr).to match(/Refusing to activate profile/)
+          apply_manifest_on(host, manifest, {:catch_changes => true})
         end
       end
     end
